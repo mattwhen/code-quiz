25a0c548ed5752859ca47da5eacfd4498110fdf2
Matthew Nguyen <76631936+mattwhen@users.noreply.github.com>
2023-01-03 07:14:39 -0600

    added renderQuestion() function that displays question from the array that is called in the startGame() function.

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,7 @@ var containerEl = document.querySelector(".content");
 var timerEl = document.querySelector('#timer');
 
 // Declare questions and initialize objects stored in a array.
-var questions = [
+var questionsArr = [
    {
         "question": "What's 2 + 2?",
         "choices": ["a", "b", "c", "d"],
@@ -28,7 +28,7 @@ var questions = [
 startBtn.addEventListener("click", startGame);
 
 // Declare countdown timer variable.
-var timer = 3;
+var timer = 60;
 
 // Start the quiz upon click and display the first question along with the countdown timer. 
 function startGame() {
@@ -38,15 +38,16 @@ function startGame() {
     // Create timer element and append to the the content container
     var createQuestion = document.createElement("div");
 
+
     // Call the countDown function using the setInterval() method, every second. 
     var timeInterval = setInterval(countdownTimer, 1000);
 
-    // When called, will begin countdown timer and will reflect changes on screen. 
+    // When called, will begin countdown timer and will render changes on screen. 
     function countdownTimer() {
-    // var createTimerSpan = document.createElement("div");
-    // createTimerSpan.setAttribute("id", "timer");
+    createQuestion.textContent = "Timer: ";
+    containerEl.appendChild(createQuestion);
     containerEl.textContent = "Timer: " + timer;
-    // containerEl.appendChild(createTimerSpan); 
+    document.querySelector(".content").setAttribute("style", "font-size: 30px;");
 
     // When timer reaches 0, it will clear timer. 
       if (timer === 0) {
@@ -56,6 +57,12 @@ function startGame() {
     }
     timer--; 
 };
-
+    renderQuestion(); 
 };
+
+function renderQuestion() {
+    var createUl = document.createElement('div');
+    createUl.textContent = questionsArr[2].question; 
+    containerEl.appendChild(createUl); 
+}
 
